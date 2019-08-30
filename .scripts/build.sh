e7667a6aceb23d438ce3030d2da9a80a6f5f4421
evil_morfar <evil_morfar@hotmail.com>
2019-08-30 19:47:19 +0200

    Updated build script

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -8,17 +8,21 @@ git submodule update --remote
 #Remove .tmp folder
 rm -r "./.tmp"
 
-# Build RCLootCouncil (locale only)
-( bash "$release_script" -t "$(pwd)/RCLootCouncil/" -r "$(pwd)/.tmp/RCLootCouncil_Classic" -p 39928 -Lz -m ".pkgmeta-rclootcouncil" )
+# Build RCLootCouncil (locale only) (and skip Libs as they shouldn't be processed with the keyword replacements)
+( bash "$release_script" -t "$(pwd)/RCLootCouncil/" -r "$(pwd)/.tmp/RCLootCouncil_Classic" -p 39928 -Lzs -m ".pkgmeta-rclootcouncil" )
+# Now just copy the original libs to the build
+robocopy "$(pwd)/RCLootCouncil/Libs/" "$(pwd)/.tmp/RCLootCouncil_Classic/RCLootCouncil/Libs" //s
 
-# Do replacements
+# # Do replacements
 . "./.scripts/replace.sh"
-
-# Build RCLootCouncil Classic
+#
+# # Build RCLootCouncil Classic
+# # -d: Skip Upload
+# # -z: Skip zip
 "$release_script" -r "$(pwd)/.tmp" -do -m ".pkgmeta-build"
-
-# Move the zip
+#
+# # Move the zip
 mv ./.tmp/*.zip "./.release/"
-
-# And delete .tmp
+#
+# # And delete .tmp
 rm -r "./.tmp"
