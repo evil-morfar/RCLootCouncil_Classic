74b74827d0c035b0799dffec5d59260bcd6a68a4
evil_morfar <evil_morfar@hotmail.com>
2020-05-18 17:04:02 +0200

    Add missing locales

--- Locale/enUS.lua
+++ Locale/enUS.lua
@@ -16,6 +16,8 @@ L["opt_autoAwardRepItemsMode_roundrobin"] = "Round Robin"
 
 L["opt_autoAwardRepItems_desc"] = "Enables auto awarding of rep items such as Coins and Bijous in Zul'Gurub."
 L["opt_autoAwardRepItemsMode_desc"] = "Choose the mode to award rep items. Personal works like normal auto awards, and round robin evenly distributes items to all players."
+L["opt_groupLoot_desc"] = "Enable this to have RCLootCouncil work with group loot when you're the group leader."
+L["opt_groupLoot_name"] = "Use with Group Loot"
 L["opt_usage_ask_ml"] = "Ask me every time I become Master Looter"
 L["opt_usage_leader_always"] = "Always use when leader"
 L["opt_usage_leader_ask"] = "Ask me when leader"
