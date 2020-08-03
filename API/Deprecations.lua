674872e7cb13e557e15b5456bf96f969c8d47cf5
evil_morfar <evil_morfar@hotmail.com>
2020-08-03 18:30:31 +0200

    Updated deprecations for changes in rclc v2.19.3

--- API/Deprecations.lua
+++ API/Deprecations.lua
@@ -1,22 +1,17 @@
 --- WoW APIs available in retail that's no longer available or changed in Classic
-function GetNumClasses ()
+function RCLootCouncil.Utils.GetNumClasses ()
 	return _G.MAX_CLASSES
 end
 
-function GetNumSpecializationsForClassID (classID)
+function GetNumSpecializationsForClassID (classID) -- REVIEW: Needed?
    -- All classes have 3 specs
    return 3
 end
 
-function GetClassInfo (classID)
+function GetClassInfo (classID) -- REVIEW: Needed?
    local info = C_CreatureInfo.GetClassInfo(classID)
    if info then
       return info.className, info.classFile, info.classID
    end
    return nil
-end
-
--- No LFG in Classic
-function IsPartyLFG ()
-   return false
 end
