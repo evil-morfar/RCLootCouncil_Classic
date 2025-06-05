b48fa87a06c42d20fb8a1d9731d21c97241a89e1
evil_morfar <10189576+evil-morfar@users.noreply.github.com>
2025-06-05 12:06:41 +0200

    MoP beta update

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -11,9 +11,10 @@ echo "Executing $0" >&2
 
 # Process command-line options
 usage() {
-	echo "Usage: test.sh [-cbp]" >&2
+	echo "Usage: test.sh [-cbdzxp]" >&2
 	echo "  -c               Pack to _classic_era_ WoW edition. (Classic)" >&2
 	echo "  -b               Pack to _classic_ WoW edition. (Cataclysm)" >&2
+	echo "  -d               Pack to _classic_beta_ WoW edition." >&2
 	echo "  -z               Pack to _classic_ptr_ WoW edition. (Cataclysm PTR)" >&2
 	echo "  -x               Pack to _classic_era_ptr_ WoW edition. (Classic PTR)" >&2
 	echo "  -p               Pack to _ptr_ WoW edition." >&2
@@ -24,12 +25,14 @@ ADDON="$(basename $ADDON_LOC)"
 WOWEDITION="_retail_"
 
 # Commandline inputs
-while getopts ":cbzpx" opt; do
+while getopts ":cbdzxp" opt; do
 	case $opt in
       c)
          WOWEDITION="_classic_era_";;
       b)
-         WOWEDITION="_classic_";;
+         WOWEDITION="_classic_";; 
+      d)
+         WOWEDITION="_classic_beta_";;
       z)
          WOWEDITION="_classic_ptr_";;
       x)
