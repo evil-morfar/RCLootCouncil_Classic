8073afc4e41d6c693e7bc4d46466caae57bdac30
evil_morfar <10189576+evil-morfar@users.noreply.github.com>
2026-03-27 03:12:43 +0100

    Added discord changelog

---
+++ .scripts/send_discord_release.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+set -e
+
+CHANGELOG_FILE=${1:-Changelog.md}
+REPO=${REPO:-"evil-morfar/RCLootCouncil_Classic"}
+WEBHOOK=${DISCORD_WEBHOOK:-""}
+
+VERSION=$(awk '/^# / { gsub(/^# /,""); print; exit }' $CHANGELOG_FILE)
+
+if [[ -z "$WEBHOOK" ]]; then
+  echo "Error: DISCORD_WEBHOOK must be set"
+  exit 1
+fi
+
+# --- Construct changelog with issue links ---
+CHANGELOG=$(sed -n "3,/^# / {/^# /d; p}" "$CHANGELOG_FILE" |
+  tr -d '\r' |
+  awk -v repo="$REPO" '
+  NF > 0 {
+    while (match($0, /\(#([0-9]+)\)/, m)) {
+      issue = m[1]
+      link = "([#" issue "](https://github.com/" repo "/pull/" issue "))"
+      $0 = substr($0, 1, RSTART-1) link substr($0, RSTART+RLENGTH)
+    }
+    print
+  }
+')
+
+# --- Truncate to 4000 chars to be safe (Discord limit 4096) ---
+CHANGELOG=$(echo "$CHANGELOG" | head -c 4000)
+
+# --- Escape newlines and quotes for JSON ---
+PAYLOAD=$(jq -n \
+    --arg version "$VERSION" \
+    --arg changelog "$CHANGELOG" \
+    --arg repo "$REPO" \
+    '{
+    username: "Release Bot",
+    avatar_url: "https://media.forgecdn.net/avatars/thumbnails/245/403/64/64/637151371641742184.png",
+    embeds: [{
+        title: ("RCLootCouncil Classic v" + $version),
+        description: ("-# A new version of RCLootCouncil Classic is available!\n" + $changelog),
+        color: 16547409,
+        footer: { text: "GitHub Actions" },
+        timestamp: now | strflocaltime("%Y-%m-%dT%H:%M:%SZ"),
+        fields: [
+            {
+                name: "Download",
+                value: ("[CurseForge](https://www.curseforge.com/wow/addons/rclootcouncil-classic/files)"
+                + " | [Wago](https://addons.wago.io/addons/rclootcouncil-classic/versions)"
+                + " | [GitHub](https://github.com/" + $repo + "/releases/latest)"),
+                inline: true
+            }
+        ]
+    }]
+    }')
+
+# --- Send to Discord ---
+curl -H "Content-Type: application/json" -d "$PAYLOAD" "$WEBHOOK"
